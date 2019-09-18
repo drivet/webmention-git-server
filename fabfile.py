64327b55d9ebf11a9e8a3938265c0269374cfae4
Desmond Rivet <desmond.rivet@gmail.com>
2019-09-17 20:43:00 -0400

    Deploy test docker instance with environment variables

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,35 @@
+import os
 from fabric import task
 
 image = 'desmondrivet/webmention-git-server:master'
-testvolume = '/var/local/webmention-test'
-volume = '/var/local/webmention'
 name = 'webmention-git-server'
 testport = 3131
 port = 3132
 
 
-@task
+env = ['ME', 'GITHUB_REPO', 'GITHUB_USERNAME', 'GITHUB_PASSWORD',
+       'WEBMENTION_FOLDER']
+
+
+def all_env_cmd():
+    return ' && '.join([env_cmd(e) for e in env])
+
+
+def env_cmd(e):
+    return 'export ' + e + '="' + os.environ[e] + '"'
+
+
+def docker_env_params():
+    return ' '.join([f'-e {e}' for e in env])
+
+
+@task(hosts=["dcr@desmondrivet.com"])
 def deploytest(c):
     c.run(f'docker pull {image}')
     c.run(f'docker stop {name}', warn=True)
-    c.run(f'docker run --name {name} -p {testport}:3031 ' +
-          f'-v {testvolume}:{volume} --rm -d {image}')
+    c.run(f'{all_env_cmd()} && docker run --name {name} ' +
+          f'{docker_env_params()} ' +
+          f'-p {testport}:3031 --rm -d {image}')
 
 
 @task
@@ -21,4 +37,4 @@ def deploy(c):
     c.run(f'docker pull {image}')
     c.run(f'docker stop {name}', warn=True)
     c.run(f'docker run --name {name} -p {port}:3031 ' +
-          f'-v {volume}:{volume} --rm -d {image}')
+          f'--rm -d {image}')
