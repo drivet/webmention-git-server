c1f9db2a2ae54660e52660ce957d08f337999ca3
Desmond Rivet <desmond.rivet@gmail.com>
2019-10-17 19:58:55 -0400

    Make only one deploy task, based on branch

--- fabfile.py
+++ fabfile.py
@@ -2,39 +2,36 @@ import os
 from fabric import task
 from fabric.transfer import Transfer
 
-image = 'desmondrivet/webmention-git-server:master'
-name = 'webmention-git-server'
-testport = 3131
-port = 3132
+branch = os.environ['CIRCLE_BRANCH']
 
+if branch == 'master':
+    port = 3132
+else:
+    port = 3131
+
+os.environ['PORT'] = port
+
+image = f'desmondrivet/webmention-git-server:{branch}'
+name = 'webmention-git-server'
 
 env = ['ME', 'GITHUB_REPO', 'GITHUB_USERNAME', 'GITHUB_PASSWORD',
        'WEBMENTION_FOLDER']
 
+extra = ['CIRCLE_BRANCH', 'PORT']
+
 
 def all_env_cmd():
-    return ' && '.join([env_cmd(e) for e in env])
+    return ' && '.join([env_cmd(e) for e in env + extra])
 
 
 def env_cmd(e):
     return 'export ' + e + '="' + os.environ[e] + '"'
 
 
-def docker_env_params():
-    return ' '.join([f'-e {e}' for e in env])
-
-
 @task(hosts=["dcr@desmondrivet.com"])
-def deploytest(c):
+@task
+def deploy(c):
     Transfer(c).put('docker-compose.yml', '/tmp')
     c.run(f'docker pull {image}')
     c.run(f'{all_env_cmd()} && ' +
           f'docker-compose -f /tmp/docker-compose.yml up -d')
-
-
-@task
-def deploy(c):
-    c.run(f'docker pull {image}')
-    c.run(f'docker stop {name}', warn=True)
-    c.run(f'docker run --name {name} -p {port}:3031 ' +
-          f'--rm -d {image}')
